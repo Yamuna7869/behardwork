20858be155462af113ad41e7918a98aaa11d6e1c
yamuna7869 <yamunadummu@gmail.com>
2024-07-02 19:04:49 +0530

    line added

--- src/beHappyForUrHappiness/MakeMotiveYourself.java
+++ src/beHappyForUrHappiness/MakeMotiveYourself.java
@@ -5,7 +5,8 @@ public class MakeMotiveYourself
 	public static void main(String[] args)
 	{
 		System.out.println("Everyone mindset is not same, Because all fingers are not equal");
-		
+	    System.out.println("All is well");
+	    System.out.println("added line");
 	}
 
 }
