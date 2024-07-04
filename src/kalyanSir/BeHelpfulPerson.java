e80393e69c561d23b5abbae599fbd0c7e3f65f91
yamuna7869 <yamunadummu@gmail.com>
2024-07-04 20:44:13 +0530

    commiting file in eclipse

---
+++ src/kalyanSir/BeHelpfulPerson.java
@@ -0,0 +1,12 @@
+package kalyanSir;
+
+public class BeHelpfulPerson {
+
+	public static void main(String[] args)
+	{
+		System.out.println("our main agenda is to give core functionalities"
+				+ " to students like primary colours");
+		System.out.println("Arif is saddist person");
+	}
+
+}
